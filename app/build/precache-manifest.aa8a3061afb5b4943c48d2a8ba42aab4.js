3f5b8bd5a727d08076df1736d947442f09e5503d
Zubair Ahmed <zubair1024@gmail.com>
2022-03-24 01:23:58 +0400

    pushing a build

---
+++ app/build/precache-manifest.aa8a3061afb5b4943c48d2a8ba42aab4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd5106080074cab716c3bcdd9353bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "325f8ca17dc00e11e449",
+    "url": "/static/css/main.5e62a0cb.chunk.css"
+  },
+  {
+    "revision": "20621c611ad618593b35",
+    "url": "/static/js/2.9c9bf062.chunk.js"
+  },
+  {
+    "revision": "8f65b69fa496b8be6ac1",
+    "url": "/static/js/3.6b342d83.chunk.js"
+  },
+  {
+    "revision": "090aff5e05cae7ff0320",
+    "url": "/static/js/4.47060385.chunk.js"
+  },
+  {
+    "revision": "325f8ca17dc00e11e449",
+    "url": "/static/js/main.7c5dca9a.chunk.js"
+  },
+  {
+    "revision": "f1887db17935557ef4c1",
+    "url": "/static/js/runtime-main.221e2654.js"
+  }
+]);
