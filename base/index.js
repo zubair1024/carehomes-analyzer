b87dd81b2aac6ba05d0e6a0c64361674f8408896
Zubair Ahmed <zubair1024@gmail.com>
2022-03-24 01:21:37 +0400

    removing api end points

--- base/index.js
+++ base/index.js
@@ -20,7 +20,7 @@ const originsWhitelist = [
   "http://localhost:3000",
   "http://localhost:3002",
   "http://localhost:8100",
-  "http://localhost:9001"
+  "http://localhost:9001",
 ];
 
 const corsOptions = {
@@ -28,7 +28,7 @@ const corsOptions = {
     let isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
     callback(null, isWhitelisted);
   },
-  credentials: true
+  credentials: true,
 };
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
@@ -62,103 +62,11 @@ app.use((req, res, next) => {
   next();
 });
 
-global.db = require("./db/db");
+// global.db = require("./db/db");
 
-db.loadModels();
+// db.loadModels();
 
-db.installConnectionHandlers();
+// db.installConnectionHandlers();
 
-db.checkConnection(() => {
-  //init routes
-  app.get("/api/test", (req, res) => {
-    res.status(200).send({
-      message: "Test looks good!"
-    });
-  });
-
-  app.get("/api/group", async (req, res) => {
-    try {
-      let groups = await db.Group.find({})
-        .lean()
-        .exec();
-      if (!groups || !groups.length) {
-        groups = [];
-      }
-      res.status(200).send({
-        status: "success",
-        data: groups
-      });
-    } catch (err) {
-      res.status(500).send({
-        message: "Internal Server Error"
-      });
-    }
-  });
-
-  app.post("/api/group", async (req, res) => {
-    try {
-      const name = req.body.name;
-      const geoJSON = req.body.geoJSON;
-      if (name && geoJSON) {
-        const newGroup  = await db.Group(req.body).save();
-        res.status(200).send({
-          status: "success",
-          data: newGroup
-        });
-      } else {
-        throw new Error("Insufficent Parameters");
-      }
-    } catch (err) {
-      res.status(500).send({
-        message: (err && err.toString()) || "Internal Server Error"
-      });
-    }
-  });
-
-  app.put("/api/group", async (req, res) => {
-    try {
-      const _id = req.body._id;
-      const name = req.body.name;
-      const geoJSON = req.body.geoJSON;
-      if (_id && name && geoJSON) {
-        const newGroup = await db.Group.updateOne(
-          { _id: _id },
-          { $set: req.body },
-          { new: true }
-        ).exec();
-        res.status(200).send({
-          status: "success",
-          data: newGroup
-        });
-      } else {
-        throw new Error("Insufficent Parameters");
-      }
-    } catch (err) {
-      res.status(500).send({
-        message: (err && err.toString()) || "Internal Server Error"
-      });
-    }
-  });
-
-  app.delete("/api/group", async (req, res) => {
-    try {
-      const _id = req.body._id;
-      if (_id) {
-        await db.Group.remove({ _id: _id }).exec();
-        res.status(200).send({
-          status: "success",
-          message: "Sucessfully Deleted"
-        });
-      } else {
-        throw new Error("Insufficent Parameters");
-      }
-    } catch (err) {
-      res.status(500).send({
-        message: (err && err.toString()) || "Internal Server Error"
-      });
-    }
-  });
-
-  app.listen(PORT);
-  console.log(`Listening on port ${PORT}`);
-});
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
